d20fc1417ddfe3f3c2f67d721ce8f56efe284326
misha <mike.afanasiuk.21@gmail.com>
2019-09-13 14:15:12 +0300

    Test6

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static('public'));
 
 app.get('/project-preview/:id', function (req, res) {
   res.set('Content-Type', 'text/html');
-  axios.get('https://dcd90e2c.ngrok.io/api/v1/project-preview/' + req.params.id)
+  axios.get('https://2b43e737.ngrok.io/project-preview/' + req.params.id)
     .then((response) => {
       console.log(response.data);
       res.send(new Buffer(response.data))
@@ -16,6 +16,18 @@ app.get('/project-preview/:id', function (req, res) {
       res.send(err)
     })
 });
+
+app.get('/post-preview/:id', function(req, res) {
+  res.set('Content-Type', 'text/html');
+  axios.get('https://2b43e737.ngrok.io/post-preview/' + req.params.id)
+    .then((response) => {
+      console.log(response.data);
+      res.send(new Buffer(response.data))
+    })
+    .catch((err) => {
+      res.send(err)
+    })
+})
 
 app.listen(port, function () {
   console.log('Example app listening on port' + port);
